2e683fd0bb0e5d0780e5070b3dfe8f2d1912be20
Si Kai Lee <a_technicolor_skye@hotmail.com>
2016-04-07 12:32:51 -0400

    Initial

--- dat/ppNetworks/CESNA_Process.py
+++ dat/ppNetworks/CESNA_Process.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 '''
 Process protein networks for CESNA
 Proc_Net processes the edges and Proc_Feat processes the features
@@ -12,37 +14,29 @@ def Proc_All(Net, Path, Mut):
 
     Nodes = {}
 
+    # Use set to catch repetitions
     Output = set()
 
     for line in Input_Net[1:]:
         line_s = line.split('\t')
+        # Checks if the nodes are in the dictionary
         if line_s[0] not in Nodes:
             Nodes[line_s[0]] = node_counter
             node_counter += 1
-        if line_s[1] not in Nodes:
-            Nodes[line_s[1]] = node_counter
+        # To account for '\n's at the end
+        if line_s[1][:-1] not in Nodes:
+            Nodes[line_s[1][:-1]] = node_counter
             node_counter += 1
-        # print str(Nodes[line_s[0]]) + '\t' + str(Nodes[line_s[1]]) + '\n'
-        Output.update([str(Nodes[line_s[0]]) + '\t' + str(Nodes[line_s[1]]) + '\n'])
-        # print Output
+        Output.update([str(Nodes[line_s[0]]) + '\t' + str(Nodes[line_s[1][:-1]]) + '\n'])
 
-    print len(Output)
+    # Need to know number of nodes for Svinet
+    print node_counter
+
+    Output = sorted(Output)
 
     with open('pp_networks_num.txt', 'w') as O:
         for edge in Output:
             O.write(edge)
-
-    # with open('pp_networks_num.txt', 'w') as O:
-    #     for line in Input_Net[1:]:
-    #         line_s = line.split('\t')
-    #         if line_s[0] not in Nodes:
-    #             Nodes[line_s[0]] = node_counter
-    #             node_counter += 1
-    #         if line_s[1] not in Nodes:
-    #             Nodes[line_s[1]] = node_counter
-    #             node_counter += 1
-    #         O.write(str(Nodes[line_s[0]]) + '\t' + str(Nodes[line_s[1]]) + '\n')
-
 
     with open(Path, 'r') as I_Path:
         Input_Path = I_Path.readlines()
@@ -50,31 +44,89 @@ def Proc_All(Net, Path, Mut):
     with open(Mut, 'r') as I_Mut:
         Input_Mut = I_Mut.readlines()
 
+    # Set count for features
     count = 1
 
-    with open('pp_feat_num.txt', 'w') as O1:
-        with open('pp_feat_name_num.txt', 'w') as O2:
-            for line in Input_Path:
-                line_s = line.split('\t')
-                O2.write(str(count) + '\t' + line_s[0] + '\n')
-                line_s[-1] = line_s[-1][:-1]
-                for val in line_s[1:]:
-                    if val not in Nodes:
-                        Nodes[val] = node_counter
-                        node_counter += 1
-                    O1.write(str(Nodes[val]) + '\t' + str(count) + '\n')
-                count += 1
+    feat_name_out = set()
+    feat_num_out = set()
+    NA = set()
 
-    with open('pp_feat_num.txt', 'a') as O1:
-        with open('pp_feat_name_num.txt', 'a') as O2:
-            # O1.write('Here starts mutation:\n')
-            O2.write(str(count) + '\t' + 'MUTATION' + '\n')
-            for line in Input_Mut[1:]:
-                line_s = line.split('\t')
-                if line_s[0] not in Nodes:
-                    Nodes[line_s[0]] = node_counter
-                    node_counter += 1
-                O1.write(str(Nodes[line_s[0]]) + '\t' + str(count) + '\n')
+    # For pathways
+    for line in Input_Path:
+        line_s = line.split('\t')
+        # Add feature name to set
+        feat_name_out.update([str(count) + '\t' + line_s[0] + '\n'])
+        # Remove stray '\n's
+        line_s[-1] = line_s[-1][:-1]
+        # Add to set only if node exists
+        for val in line_s[1:]:
+            # print val
+            try:
+                # print Nodes[val][2]
+                feat_num_out.update([str(Nodes[val]) + '\t' + str(count) + '\n'])
+                NA.update([(Nodes[val], count)])
+                # print NA
+            except KeyError:
+                pass
+        # Increase count
+        count += 1
+
+    # For mutations
+    for line in Input_Mut[1:]:
+        line_s = line.split('\t')
+        feat_name_out.update([str(count) + '\t' + 'MUTATION' + '\n'])
+        # Write only if node exists
+        if line_s[0] in Nodes:
+            feat_num_out.update([str(Nodes[line_s[0]]) + '\t' + str(count) + '\n'])
+            NA.update([(Nodes[line_s[0]], count)])
+
+    print node_counter, count
+    NA_array = np.zeros((node_counter, count))
+    for i in NA:
+        NA_array[i[0]-1, i[1]-1] = 1
+
+    with open('pp_feat_num.txt', 'w') as O1:
+        for i in feat_num_out:
+            O1.write(i)
+
+    with open('pp_feat_name.txt', 'w') as O2:
+        for i in feat_name_out:
+            O2.write(i)
+
+    with open('NA.txt', 'w') as O3:
+        for i in NA_array:
+            O3.write(str(i) + '\n')
+
+    return NA_array
+    # Doesn't account for duplications
+    # # For pathways
+    # with open('pp_feat_num.txt', 'w') as O1:
+    #     with open('pp_feat_name_num.txt', 'w') as O2:
+    #         for line in Input_Path:
+    #             line_s = line.split('\t')
+    #             # Write feature name
+    #             O2.write(str(count) + '\t' + line_s[0] + '\n')
+    #             # Remove stray '\n's
+    #             line_s[-1] = line_s[-1][:-1]
+    #             # Write only if node exists
+    #             for val in line_s[1:]:
+    #                 try:
+    #                     O1.write(str(Nodes[val]) + '\t' + str(count) + '\n')
+    #                 except KeyError:
+    #                     pass
+    #             # Increase count
+    #             count += 1
+
+    # # For mutations
+    # with open('pp_feat_num.txt', 'a') as O1:
+    #     with open('pp_feat_name_num.txt', 'a') as O2:
+    #         # Write feature name
+    #         O2.write(str(count) + '\t' + 'MUTATION' + '\n')
+    #         for line in Input_Mut[1:]:
+    #             line_s = line.split('\t')
+    #             # Write only if node exists
+    #             if line_s[0] in Nodes:
+    #                 O1.write(str(Nodes[line_s[0]]) + '\t' + str(count) + '\n')
 
 
 # def Proc_Net(File):
